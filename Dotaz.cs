788e1e3c8a0752ff234ae50e378fd71f2836cb6c
dortozrout <zhunal7@gmail.com>
2022-06-22 14:15:03 +0200

    Rozpracovani dotazu

---
+++ Dotaz.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TiskStitku
+{
+
+	public class Dotaz
+	{
+		public string Otazka { get; private set; }
+		public string Odpoved { get; private set; }
+		
+
+	}
+}
--- eplPrikaz.cs
+++ eplPrikaz.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Text;
+using System.Collections.Generic;
 //using System.Net;
 //using System.Net.Sockets;
 //using System.IO;
@@ -10,6 +11,7 @@ namespace TiskStitku
 	{
 		public string NazevSouboru { get; private set; }
 		public string Telo { get; private set; }
+		public List<Dotaz> SeznamDotazu { get; private set; } 
 		public EplPrikaz(string nazevSouboru, string telo)
 		{
 			NazevSouboru = nazevSouboru;
