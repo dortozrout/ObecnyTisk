c2cbbb3d5525af7ea0a8c519bac62eb90f588d73
dortozrout <zhunal7@gmail.com>
2024-07-28 21:54:54 +0200

    InputForm displays template

--- Form/InputForm.cs
+++ Form/InputForm.cs
@@ -7,7 +7,8 @@ namespace Form
     public class InputForm<T> : ConsoleForm
     {
         private Background Background { get; set; }
-        private FieldReadOnly EplFileSpec { get; set; }
+        private FieldReadOnly EplFileName { get; set; }
+        private FieldReadOnly EplTemplate { get; set; }
         private UInputField<T> InputField { get; set; }
         public InputForm()
         {
@@ -19,12 +20,14 @@ namespace Form
             Console.Clear();
             Console.CursorVisible = true;
             Background.Display();
-            EplFileSpec.Display();
+            EplFileName.Display();
+            EplTemplate.Display();
             InputField.Display();
         }
         public T Fill(EplFile eplFile, string question, string defaultText)
         {
-            EplFileSpec = new FieldReadOnly("eplfilespec", 3, 10, string.Format("Tisk souboru: {0}", eplFile.JmenoSouboru), 20, this);
+            EplFileName = new FieldReadOnly("eplfilespec", 3, 7, string.Format("Tisk souboru: {0}", eplFile.JmenoSouboru), 20, this);
+            EplTemplate = new FieldReadOnly("epltemplate", 3, 9, string.Format("Šablona:\n{0}", eplFile.Sablona), 20, this);
             AditionalParms ap = new AditionalParms()
             {
                 switchToNext = new List<ConsoleKeyInfo>() { new ConsoleKeyInfo('\r', ConsoleKey.Enter, false, false, false) },
