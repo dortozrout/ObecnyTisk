db003c1666944d1ca28735beedff1230111272b9
dortozrout <zhunal7@gmail.com>
2025-01-30 13:33:51 +0100

    removed version number from exe name

--- compile.bat
+++ compile.bat
@@ -1,6 +1,6 @@
 @echo off
 rem C:\Windows\Microsoft.NET\Framework64\v4.0.30319\csc.exe -nologo -r:System.Net.Http.dll  *.cs HtmlParser\*.cs Form\*.cs
-set exeName=Labels_202501.exe
+set exeName=Labels.exe
 set currentDisc=%cd:~0,3%
 IF EXIST "%currentDisc%Roslyn-4.1.0\csc.exe" (
     %currentDisc%Roslyn-4.1.0\csc.exe -nologo -r:System.Net.Http.dll *.cs .\Form\*.cs -out:%exeName%
