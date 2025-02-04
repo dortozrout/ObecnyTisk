f945ff018458db7126c7a14fe12ed9b573e24e12
dortozrout <zhunal7@gmail.com>
2025-02-04 10:52:55 +0100

    bat file for publishing selfcontained version

--- compile.bat
+++ compile.bat
@@ -1,9 +1,13 @@
 @echo off
 rem C:\Windows\Microsoft.NET\Framework64\v4.0.30319\csc.exe -nologo -r:System.Net.Http.dll  *.cs HtmlParser\*.cs Form\*.cs
 set exeName=Labels.exe
+IF NOT EXIST .\publish (
+    mkdir publish
+)
+
 set currentDisc=%cd:~0,3%
 IF EXIST "%currentDisc%Roslyn-4.1.0\csc.exe" (
-    %currentDisc%Roslyn-4.1.0\csc.exe -nologo -r:System.Net.Http.dll *.cs .\Form\*.cs -out:%exeName%
+    %currentDisc%Roslyn-4.1.0\csc.exe -nologo -r:System.Net.Http.dll *.cs .\Form\*.cs -out:.\publish\%exeName%
 ) ELSE (
     echo Compiller not found!
 )
